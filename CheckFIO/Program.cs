b2bc7cf4796099a9554ef29b69eea094cf67f23a
Alex Sasov <sasov1993@mail.ru>
2021-09-16 20:11:57 +0300

    сравнение строк

---
+++ CheckFIO/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using StrignComporation;
+
+namespace CheckFIO
+{
+    class Program
+    {
+        static List<string> LoadUsersSQL()
+        {
+            var fio = new List<string>();
+            using (var sr = new StreamReader("D:\\users.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var arr = line.Split(';');
+
+                    fio.Add(arr[2].Replace('"', ' ').Trim());
+                }
+            }
+
+            return fio;
+        }
+
+        static List<string> LoadUsersProgress()
+        {
+            var fio = new List<string>();
+            using (var sr = new StreamReader("D:\\progress.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var arr = line.Split(';');
+
+                    fio.Add(arr[0].Trim());
+                }
+            }
+
+            return fio;
+        }
+
+        static void AddLog(string log)
+        {
+            using (var sw = new StreamWriter("D:\\checkUsers1.csv", true))
+            {
+                sw.WriteLine(log);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var sql = LoadUsersSQL();
+            var prog = LoadUsersProgress();
+
+            int minIndex = 0;
+            double cur, min = double.MaxValue;
+
+            var sqlL = sql.OrderBy(x => x);
+            foreach(var it in sqlL)
+            {
+                min = double.MaxValue;
+                for (int i=0;i<prog.Count;i++)
+                {
+                    var it1 = prog[i];
+
+                    cur = it.D(it1);
+                    if (min > cur)
+                    {
+                        min = cur;
+                        minIndex = i;
+                    }
+                }
+
+                if (min > 0)
+                    AddLog($"{it},{prog[minIndex]},{min}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StrignComporation/Program.cs
@@ -0,0 +1,63 @@
+﻿using F23.StringSimilarity;
+using System;
+
+namespace StrignComporation
+{
+    public static class Ext
+    {
+        public static double D(this string s, string s1)
+        {
+            var l = new Damerau();
+
+            var sArr = s.Split(" ");
+            var sArr1 = s1.Split(" ");
+
+            double d = 0;
+            double min;
+            int minIndex = 0;
+            double cur;
+
+            foreach (var it in sArr)
+            {
+                min = double.MaxValue;
+                for(int i = 0;i<sArr1.Length;i++)
+                {
+                    cur = l.Distance(it, sArr1[i]);
+
+                    if (cur < min)
+                    {
+                        min = cur;
+                        minIndex = i;
+                    }
+                }
+                d += min;
+            }
+
+
+            return d;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new QGram();
+
+            int index = 0;
+
+            Console.WriteLine("{0}: {1}", ++index, "Иванов Иван Иванович".D("Иван Иванов"));
+            Console.WriteLine("{0}: {1}", ++index, "Петров Иван Иванович".D("Иван Иванов"));
+            Console.WriteLine("{0}: {1}", ++index, "Иванов Алексей Иванович".D("Иван Иванов"));
+            Console.WriteLine("{0}: {1}", ++index, "Сасов Александр".D("Александр Викторович Сасов"));
+            Console.WriteLine("{0}: {1}", ++index, "Сасов Александр Викторович".D("Александр Викторович Сасов"));
+            Console.WriteLine("{0}: {1}", ++index, "Сасов Виктор Васильевич".D("Александр Викторович Сасов"));
+            Console.WriteLine("{0}: {1}", ++index, "Часов Александр Викторович".D("Александр Викторович Сасов"));
+            Console.WriteLine("{0}: {1}", ++index, "Сасов Александр".D("Александр Викторович Костров"));
+            Console.WriteLine("{0}: {1}", ++index, "Сасов Александр".D("Дмитрий Викторович Сасов"));
+            Console.WriteLine("{0}: {1}", ++index, "Иванов Иван Иванович".D("Иванов Иван Иванович"));
+
+            Console.ReadLine();
+        }
+    }
+}
