868cd84c2abf7f275d217e8422f7916374ec9ac8
Alex Sasov <sasov1993@mail.ru>
2021-09-19 20:31:38 +0300

    поппытка перенести логику в атрибут

---
+++ StrignComporation/DtFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Web.Http.Filters;
+
+namespace StrignComporation
+{
+    public class DtFilterAttribute : ActionFilterAttribute
+    {
+        private Stopwatch sw = new Stopwatch();
+
+        public void OnResultExecuted()
+        {
+            sw.Stop();
+            Console.WriteLine("Time {0}", sw.ElapsedMilliseconds);
+        }
+
+        public void OnResultExecuting()
+        {
+            sw.Start();
+        }
+
+        public void OnActionExecuting()
+        {
+            sw.Start();
+        }
+    }
+}
