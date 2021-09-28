77882f48e0d38278383b3d30e51ede3246bdd021
Alex Sasov <sasov1993@mail.ru>
2021-09-28 19:21:42 +0300

    правки по elastic, дописал круд надстройку

--- Elastic/IElasticSearchActions.cs
+++ Elastic/IElasticSearchActions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Elastic
+﻿namespace Elastic
 {
-    interface IElasticSearchActions<T>
+    interface IElasticActions<T>
     {
         bool Create(T instance);
         bool Update(T instance);
--- Elastic/Program.cs
+++ Elastic/Program.cs
@@ -1,21 +1,67 @@
-﻿using Elasticsearch.Net;
-using Nest;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using System;
+using System.Threading.Tasks;
 
 namespace Elastic
 {
+
+    public class ESActionsTest
+    {
+        private Lazy<ESActions<Test>> _lazyActions = new Lazy<ESActions<Test>>(() => new ESActions<Test>());
+        public ESActions<Test> Actions => _lazyActions.Value;
+
+        private Test getTest(int id)
+        {
+            id = id < 10000 ? id + 10000 : id;
+
+            return new Test()
+            {
+                Id = id,
+                Title = $"title {id}",
+                Description = $"description {id}"
+            };
+        }
+
+        [Benchmark]
+        public void ESCreateTest()
+        {
+            Actions.Create(getTest(101));
+        }
+
+        [Benchmark]
+        public void ESUpdateTest()
+        {
+            Actions.Create(getTest(101));
+        }
+
+        [Benchmark]
+        public void ESDeleteTest()
+        {
+            Actions.Delete(getTest(101));
+        }
+    }
+
     class Program
     {
+        static void BenchmarkRunTestActionTime()
+        {
+            var summary = BenchmarkRunner.Run<ESActionsTest>();
+        }
+
         static void Main(string[] args)
         {
-            var test = new Test();
-            //var response = ES.ESClient.Get<Test>(1, idx => idx.Index(test.GetIndex)); // returns an IGetResponse mapped 1-to-1 with the Elasticsearch JSON response
-            //test = response.Source; // the original document
+            var tsk = new Task(BenchmarkRunTestActionTime);
+            tsk.Start();
 
-            var actions = new ESTestActions<Test>();
+            var test = new Test();
+            var response = ES.ESClient.Get<Test>(1, idx => idx.Index(test.GetIndex)); // returns an IGetResponse mapped 1-to-1 with the Elasticsearch JSON response
+            test = response.Source; // the original document
+
+            var actions = new ESActions<Test>();
             actions.Create(new Test()
             {
-                Id = 7,
+                Id = 8,
                 Title = "c# .net auto create",
                 Description = "auto create elastic search"
             });
