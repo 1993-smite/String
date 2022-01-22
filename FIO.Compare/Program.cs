bfdddd3395b53aac5d868efe9b09cc2586376d43
Alex Sasov <sasov1993@mail.ru>
2022-01-23 00:28:23 +0300

    поменял на Caverphone

--- FIO.Compare/Program.cs
+++ FIO.Compare/Program.cs
@@ -15,6 +15,8 @@ namespace FIO.Compare
         public static List<User> Users
             = new List<User>();
 
+        public static int Cnt = 0;
+
         static void Load()
         {
             using (var sr = new StreamReader(@"D:\progress4.csv"))
@@ -24,7 +26,7 @@ namespace FIO.Compare
                     var line = sr.ReadLine();
                     if (line.Length > 2)
                     {
-                        Users.Add(new User(line.Replace("?", "")));
+                        Users.Add(new User(line.ToLower().Filter().Replace("?", "")));
                     }
                 }
             }
@@ -34,20 +36,35 @@ namespace FIO.Compare
         {
             Load();
             Parallel.For(0, Users.Count - 1, Compare);
+
+            //Console.WriteLine("test".IsEnglish());
+            //Console.WriteLine("test ggg".IsEnglish());
+            //Console.WriteLine("test9".IsEnglish());
+            //Console.WriteLine("апра".IsEnglish());
+            //Console.WriteLine("апра авпв".IsEnglish());
+            //Console.WriteLine("апра efgdf".IsEnglish());
+
             Console.WriteLine("The End!");
         }
 
         static void Compare(int from)
         {
+            Cnt++;
+
+            if (Cnt % 100 == 0)
+                Console.WriteLine(Cnt);
+
             var user = Users[from];
             for (int to = from + 1; to < Users.Count; to++)
             {
                 var toUser = Users[to];
 
-                var d = user.FIO.Eng().Metaphone(toUser.FIO.Eng());
+                double d = user.FIO.Eng()
+                    .Caverphone(toUser.FIO.Eng());
+
                 if (d < 1)
                 {
-                    Logger.Info($"{from};{user.FIO.Eng()};{to};{toUser.FIO.Eng()};{d}");
+                    Logger.Info($"{from};{user.FIO};{to};{toUser.FIO};{d}");
                 }
 
             }
